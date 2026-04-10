7af6c7e82d667b57198057f7153758c968f652e2
Jonas Schleich <jonas.schleich@student.tugraz.at>
2026-04-10 20:40:59 +0200

    added more important dates

--- frontend/plotsUI.py
+++ frontend/plotsUI.py
@@ -19,7 +19,7 @@ def plotsSite():
     if "query" not in st.session_state:
         st.session_state.query = ""
     if "file" not in st.session_state:
-        st.session_state.file = ""
+        st.session_state.file = "Posts.parquet"
     if "dates" not in st.session_state:
         st.session_state.dates = True
     if "rolling" not in st.session_state:
@@ -43,6 +43,11 @@ def plotsSite():
     with col_right:
         selected_dates = st.date_input("Select Timespan:",value=[datetime.date(2022, 1, 1), datetime.date(2023, 1, 1)], min_value=datetime.date(2009, 1, 1), max_value=datetime.date(2026, 1, 1))
         
+    with st.expander("Select release dates to be included in the plot", expanded=True):
+                selected_map = {}
+                for entry in IMPORTANTDATES.keys():
+                    selected_map[entry] = st.checkbox(f"{entry} - {IMPORTANTDATES.get(entry)}", value=True)
+    
     if(st.session_state.path != ""):
         try:
             files = [f for f in os.listdir(st.session_state.path) if f.lower().endswith('.parquet')]
@@ -57,23 +62,32 @@ def plotsSite():
         if(st.session_state.file != ""):
             try:
                 start_date, end_date = selected_dates
-                file_path = os.path.join(st.session_state.path, st.session_state.file)
-                queries = [f"SELECT CAST(CreationDate AS DATE) AS PostDate, COUNT(*) AS PostCount FROM '{file_path}' WHERE PostTypeId = 1 GROUP BY PostDate ORDER BY PostDate",
-                           f"SELECT CAST(CreationDate AS DATE) AS PostDate, COUNT(*) AS PostCount FROM '{file_path}' WHERE PostTypeId = 2 GROUP BY PostDate ORDER BY PostDate"]
+                filepath = os.path.join(st.session_state.path, st.session_state.file)
+                queries = [f"SELECT CAST(CreationDate AS DATE) AS PostDate, COUNT(*) AS PostCount FROM '{filepath}' WHERE PostTypeId = 1 GROUP BY PostDate ORDER BY PostDate",
+                           f"SELECT CAST(CreationDate AS DATE) AS PostDate, COUNT(*) AS PostCount FROM '{filepath}' WHERE PostTypeId = 2 GROUP BY PostDate ORDER BY PostDate",
+                           f"WITH TopTags AS (SELECT tag FROM (SELECT unnest(string_split(trim(Tags, '<>'), '><')) AS tag FROM '{filepath}' WHERE PostTypeId = 1 AND Tags IS NOT NULL) GROUP BY tag ORDER BY COUNT(*) DESC LIMIT 10),MonthlyTags AS (SELECT date_trunc('month', CreationDate) AS PostMonth,unnest(string_split(trim(Tags, '<>'), '><')) AS tag FROM '{filepath}'WHERE PostTypeId = 1 AND Tags IS NOT NULL)SELECT CAST(PostMonth AS DATE) AS PostDate, tag AS TagName,COUNT(*) AS TagCount FROM MonthlyTags WHERE tag IN (SELECT tag FROM TopTags) GROUP BY PostDate, TagName ORDER BY PostDate"
+                           ]
                 st.session_state.query = selectboxWrapper("Select a predefined query:", queries, "")
                 if(st.session_state.query != ""):
                     df = executeCustomQueryDF(st.session_state.query)
                     if df.empty:
                         st.warning("Query is empty")
                     else:
-                        df["PostDate"] = pd.to_datetime(df["PostDate"])
-                        df[f"{st.session_state.rolling} day trend"] = df["PostCount"].rolling(window=st.session_state.rolling, min_periods=1).mean()
-                        fig = px.line(df, x="PostDate", y=["PostCount", f"{st.session_state.rolling} day trend"])
+                        if "TagName" in df.columns:
+                            fig = px.line(df, x="PostDate", y="TagCount", color="TagName")
+                            fig.update_traces(line_width=2)
+                        else:
+                            df["PostDate"] = pd.to_datetime(df["PostDate"])
+                            df[f"{st.session_state.rolling} day trend"] = df["PostCount"].rolling(window=st.session_state.rolling, min_periods=1).mean()
+                            fig = px.line(df, x="PostDate", y=["PostCount", f"{st.session_state.rolling} day trend"])
+                            
+                            fig.data[1].line.color = 'red'
+                            fig.data[1].line.width = 3
                         fig.update_xaxes(range=[pd.to_datetime(start_date), pd.to_datetime(end_date)])
-                        fig.data[1].line.color = 'red'
-                        fig.data[1].line.width = 3
+                        fig.update_layout(uirevision=str(selected_dates))
+                        st.session_state.last_dates = selected_dates
                         for event_name, event_date in IMPORTANTDATES.items():
-                            if(st.session_state.dates):
+                            if(st.session_state.dates and selected_map[event_name]):
                                 date_ms = pd.Timestamp(event_date).timestamp() * 1000
                                 fig.add_vline(
                                     x=date_ms, 
--- shared/defines.py
+++ shared/defines.py
@@ -1,4 +1,31 @@
 import datetime
 
 IGNOREDDIRECTORIES = [".venv", ".git", "__pycache__", "backend", "frontend"]
-IMPORTANTDATES = {"ChatGPT": datetime.date(2022,11,30)}
+
+OPENAIRELEASES = {"ChatGPT-3.5": datetime.date(2022,11,30),         #https://en.wikipedia.org/wiki/GPT-3#GPT-3.5
+                  "ChatGPT-4": datetime.date(2023,3,14),            #https://en.wikipedia.org/wiki/GPT-4
+                  "ChatGPT-4o": datetime.date(2024,5,13),           #https://en.wikipedia.org/wiki/GPT-4o
+                  "ChatGPT o1": datetime.date(2024,12,5),           #https://en.wikipedia.org/wiki/OpenAI_o1
+                  "ChatGPT-4.5": datetime.date(2025,2,27),          #https://en.wikipedia.org/wiki/GPT-4.5
+                  "ChatGPT-4.1": datetime.date(2025,4,14),          #https://en.wikipedia.org/wiki/GPT-4.1
+                  "ChatGPT o3": datetime.date(2025,4,16),           #https://en.wikipedia.org/wiki/OpenAI_o3
+                  "ChatGPT-5": datetime.date(2025,8,7),             #https://en.wikipedia.org/wiki/GPT-5
+                  "ChatGPT-5.1": datetime.date(2025,11,12),         #https://en.wikipedia.org/wiki/GPT-5.1
+                  "ChatGPT-5.2": datetime.date(2025,12,11),         #https://en.wikipedia.org/wiki/GPT-5.2
+                  "ChatGPT-5.4": datetime.date(2026,3,5),}          #https://en.wikipedia.org/wiki/GPT-5.4
+
+
+#Only major releases are here because it would clutter everything otherwise
+GOOGLERELEASES = {"Google Bard": datetime.date(2023,3,21),          #https://en.wikipedia.org/wiki/Gemini_(language_model)#Model_versions
+                  "Gemini 1.0 Pro": datetime.date(2024,5,13),       #https://en.wikipedia.org/wiki/Gemini_(language_model)#Model_versions      
+                  "Gemini 1.5 Pro": datetime.date(2025,2,27),       #https://en.wikipedia.org/wiki/Gemini_(language_model)#Model_versions
+                  "Gemini 2.0 Pro": datetime.date(2025,4,16),       #https://en.wikipedia.org/wiki/Gemini_(language_model)#Model_versions
+                  "Gemini 2.5 Pro": datetime.date(2025,8,7),        #https://en.wikipedia.org/wiki/Gemini_(language_model)#Model_versions
+                  "Gemini 3 Pro": datetime.date(2025,11,12),        #https://en.wikipedia.org/wiki/Gemini_(language_model)#Model_versions
+                  "Gemini GPT-5.2": datetime.date(2025,12,11),      #https://en.wikipedia.org/wiki/Gemini_(language_model)#Model_versions
+                  "Gemini GPT-5.4": datetime.date(2026,3,5),}       #https://en.wikipedia.org/wiki/Gemini_(language_model)#Model_versions
+
+
+COVID = {"COVID:": datetime.date(2020,3,1)}                         #WHO declares pandemic https://www.nm.org/healthbeat/medical-advances/new-therapies-and-drug-trials/covid-19-pandemic-timeline#:~:text=COVID%2D19%20Pandemic%20Timeline,what%20happened%20along%20the%20way.
+
+IMPORTANTDATES = OPENAIRELEASES | GOOGLERELEASES | COVID
