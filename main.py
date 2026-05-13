4662c8e3667e98a8cb6e74dc1017d41bb3b57c32
Fluadl <florianhoedl5@gmail.com>
2026-05-13 12:17:42 +0200

    Changed Debbug Messages

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ def main():
     """
     while True:
         print("Please select on what you would like to do:")
-        print("0 Exit")
-        print("1 Parse XML files to parquet files")
-        print("2 Describe all tables")
-        print("3 Cast tables to correct types (creates new files called [tableName]_typed.parquet)")
-        print("4 Calculate complexity of a file")
+        print("0: Exit")
+        print("1: Parse XML files to parquet files")
+        print("2: Describe all tables")
+        print("3: Cast tables to correct types (creates new files called [tableName]_typed.parquet)")
+        print("4: Calculate complexity of a file")
         
         action = input()
         if(action == "0"):
